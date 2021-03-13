af959c04a177a66209ee486df7ab58cf676b7217
Андрей Кормаченко <andreysxkormachenko@gmail.com>
2021-03-13 20:40:02 +0300

    videos

--- android/app/src/main/java/house/with/swimmingpool/ui/videos/listvideos/VideosListFragment.kt
+++ android/app/src/main/java/house/with/swimmingpool/ui/videos/listvideos/VideosListFragment.kt
@@ -1,6 +1,30 @@
 package house.with.swimmingpool.ui.videos.listvideos
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import house.with.swimmingpool.R
+import house.with.swimmingpool.models.Video
+import house.with.swimmingpool.ui.home.adapters.VideosAdapter
 
-class VideosListFragment : Fragment(R.layout.fragment_video_list)
+class VideosListFragment : Fragment(R.layout.fragment_video_list) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<View>(R.id.backIcon).setOnClickListener {
+            findNavController().popBackStack()
+        }
+
+        view.findViewById<RecyclerView>(R.id.videosRV).apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = VideosAdapter(listOf(Video(), Video(), Video())) {
+                findNavController().navigate(R.id.action_videosListFragment_to_videoFragment)
+            }
+        }
+    }
+
+}
