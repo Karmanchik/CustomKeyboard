c09357ee69dd174ab992fdc54c8f7b8f927275d4
Андрей Кормаченко <andreysxkormachenko@gmail.com>
2021-03-13 17:57:06 +0300

    news list

--- android/app/src/main/java/house/with/swimmingpool/ui/news/listnews/NewsListFragment.kt
+++ android/app/src/main/java/house/with/swimmingpool/ui/news/listnews/NewsListFragment.kt
@@ -1,6 +1,25 @@
 package house.with.swimmingpool.ui.news.listnews
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import house.with.swimmingpool.R
+import house.with.swimmingpool.models.News
+import house.with.swimmingpool.ui.home.adapters.NewsAdapter
 
-class NewsListFragment : Fragment(R.layout.fragment_news_list)
+class NewsListFragment : Fragment(R.layout.fragment_news_list) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<RecyclerView>(R.id.newsRV).apply {
+            layoutManager = GridLayoutManager(context, 2)
+            adapter = NewsAdapter(listOf(News(), News(), News(), News()), requireContext()) {
+                findNavController().navigate(R.id.action_newsListFragment_to_newsSingleFragment)
+            }
+        }
+    }
+
+}
