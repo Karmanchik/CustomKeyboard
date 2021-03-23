926eaa127a2625a685db7fc990538b2243eaa8c0
jon-kingston <artstim123@gmail.com>
2021-03-23 19:56:48 +0300

    tmp

--- android/app/src/main/java/house/with/swimmingpool/ui/house/HouseFragment.kt
+++ android/app/src/main/java/house/with/swimmingpool/ui/house/HouseFragment.kt
@@ -1,18 +1,53 @@
 package house.with.swimmingpool.ui.house
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import house.with.swimmingpool.R
+import house.with.swimmingpool.databinding.FragmentHomeBinding
+import house.with.swimmingpool.databinding.FragmentHouseBinding
 import house.with.swimmingpool.models.HouseCatalogData
+import house.with.swimmingpool.models.MainTags
+import house.with.swimmingpool.ui.favourites.adapters.TagAdapter
+import house.with.swimmingpool.ui.house.adapters.HouseHeaderAdapter
 
-class HouseFragment : Fragment(R.layout.fragment_house) {
+class HouseFragment : Fragment() {
+
+    private var houseObjectBinding: FragmentHouseBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        houseObjectBinding = FragmentHouseBinding.inflate(layoutInflater)
+
+        return houseObjectBinding?.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-//        if ()
         val house = arguments?.getSerializable("house") as? HouseCatalogData
+
+        houseObjectBinding?.apply {
+            val vp = mainHousesContainer
+            vp.adapter = HouseHeaderAdapter(listOf("", "", ""))
+
+            dotsIndicator.setViewPager2(vp)
+
+            hashTagRV.adapter = TagAdapter(requireContext(), listOf(
+                MainTags("#4890FB", "Ипотека"),
+                MainTags("#4890FB", "Ипотека"),
+                MainTags("#4890FB", "Ипотека"))
+            )
+        }
     }
 
+    override fun onDestroy() {
+        houseObjectBinding = null
+        super.onDestroy()
+    }
 }
