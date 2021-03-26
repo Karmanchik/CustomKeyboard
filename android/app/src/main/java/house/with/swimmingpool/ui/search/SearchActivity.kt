0ce6a41101195bc317009fd3c25d652ebbca63bd
jon-kingston <artstim123@gmail.com>
2021-03-26 18:32:06 +0300

    tmp

---
+++ android/app/src/main/java/house/with/swimmingpool/ui/search/SearchActivity.kt
@@ -0,0 +1,12 @@
+package house.with.swimmingpool.ui.search
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import house.with.swimmingpool.R
+
+class SearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+    }
+}
