07399c4cc2b9f0744990416932713045b6763e90
Андрей Кормаченко <andreysxkormachenko@gmail.com>
2021-04-17 14:09:16 +0300

    show keyboard in save filter

--- android/app/src/main/java/house/with/swimmingpool/ui/savefilter/SaveFilterFragment.kt
+++ android/app/src/main/java/house/with/swimmingpool/ui/savefilter/SaveFilterFragment.kt
@@ -1,10 +1,15 @@
 package house.with.swimmingpool.ui.savefilter
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Context.INPUT_METHOD_SERVICE
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.DialogFragment
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
@@ -13,6 +18,7 @@ import house.with.swimmingpool.R
 import house.with.swimmingpool.api.config.controllers.RealtyServiceImpl
 import house.with.swimmingpool.databinding.RangeSaveFilterBinding
 import house.with.swimmingpool.ui.toast
+
 
 class SaveFilterFragment : BottomSheetDialogFragment() {
 
@@ -35,6 +41,8 @@ class SaveFilterFragment : BottomSheetDialogFragment() {
     @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.name.requestFocus()
 
         binding.apply {
             closeIcon.setOnClickListener { dismiss() }
